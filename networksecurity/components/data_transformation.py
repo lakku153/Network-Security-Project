3ef71ce0925c7ca4fafa580094253fc80813c611
Lokesh Kumar Saini <lokesh726888@gmail.com>
2025-01-22 20:17:09 +0530

    data transformation implemented

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -69,8 +69,23 @@ class DataTransformation:
 
             preprocessor=self.get_data_transformer_object()
             preprocessor_object=preprocessor.fit(input_feature_train_df)
-            # transformed_input_train_featu
+            transformed_input_train_feature=preprocessor_object.transform(input_feature_train_df)
+            transformed_input_test_feature=preprocessor_object.transform(input_feature_test_df)
+            train_arr=np.c_[transformed_input_train_feature,np.array(target_feature_train_df)]
+            test_arr=np.c_[transformed_input_test_feature,np.array(target_feature_test_df)]
 
+            #save numpy array data
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path,array=train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path,array=test_arr)
+            save_object(self.data_transformation_config.transformed_object_file_path,preprocessor_object)
+
+            #preparing artifacts
+            data_transformation_artifact=DataTransformationArtifact(
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path,
+            )
+            return data_transformation_artifact
 
         except Exception as e:
             raise NetworkSecurityException(e,sys)
