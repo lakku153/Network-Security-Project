86a37462d6836b460ffb4ea631d90591b4fdde70
Lokesh Kumar Saini <lokesh726888@gmail.com>
2025-01-26 18:22:02 +0530

    training pipeline implemented

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -49,3 +49,40 @@ class TrainingPipeline:
 
             except Exception as e:
                 raise NetworkSecurityException(e,sys)
+        def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
+            try:
+                self.data_transformation_config=DataValidationConfig(training_pipeline_config=self.trainig_pipeline_config)
+                logging.info("Start data transformation")
+                data_transformation=DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=self.data_transformation_config)
+                data_transformation_artifact=data_transformation.initiate_data_transformation()
+                logging.info(f"Data transformation completed and artifact: {data_transformation_artifact}")
+                return data_transformation_artifact
+
+            except Exception as e:
+                raise NetworkSecurityException(e,sys)
+
+        def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
+            try:
+                self.model_trainer_config:ModelTrainerConfig=ModelTrainerConfig(
+                    trainig_pipeline_config=self.trainig_pipeline_config
+                )
+                logging.info("started the model traininig")
+                model_trainer=ModelTrainer(
+                    data_transformation_artifact=data_transformation_artifact,
+                    model_trainer_config=self.model_trainer_config
+                )
+                model_trainer_artifact=model_trainer.initiate_model_trainer()
+                logging.info(f"model trainingcompleted and artifact: {model_trainer_artifact}")
+                return model_trainer_artifact
+            except Exception as e:
+                return NetworkSecurityException(e,sys)
+
+        def run_pipeline(self):
+            try:
+                data_ingestion_artifact=self.start_data_ingestion()
+                data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+                data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+                model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+                return model_trainer_artifact
+            except Exception as e:
+                raise NetworkSecurityException(e,sys)
