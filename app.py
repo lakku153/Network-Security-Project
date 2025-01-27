4dd19cdebed3d3466d8b95fd1918231acfa98196
lakku153 <lokesh726888@gmail.com>
2025-01-27 09:05:44 +0530

    pipeline is completed]

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 import certifi
-# ca = certifi.where()
+ca = certifi.where()
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,12 +20,12 @@ from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
 
-# from networksecurity.utils.main_utils.utils import load_object
+from networksecurity.utils.main_utils.utils import load_object
 
-# from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
 
-client = pymongo.MongoClient(mongo_db_url, tlsCAFile)
+client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 
 from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME
 from networksecurity.constant.training_pipeline import DATA_INGESTION_DATABASE_NAME
@@ -44,8 +44,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# from fastapi.templating import Jinja2Templates
-# templates = Jinja2Templates(directory="./templates")
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
 
 @app.get("/", tags=["authentication"])
 async def index():
@@ -60,28 +60,30 @@ async def train_route():
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
-# @app.post("/predict")
-# async def predict_route(request: Request,file: UploadFile = File(...)):
-#     try:
-#         df=pd.read_csv(file.file)
-#         #print(df)
-#         preprocesor=load_object("final_model/preprocessor.pkl")
-#         final_model=load_object("final_model/model.pkl")
-#         network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
-#         print(df.iloc[0])
-#         y_pred = network_model.predict(df)
-#         print(y_pred)
-#         df['predicted_column'] = y_pred
-#         print(df['predicted_column'])
-#         #df['predicted_column'].replace(-1, 0)
-#         #return df.to_json()
-#         df.to_csv('prediction_output/output.csv')
-#         table_html = df.to_html(classes='table table-striped')
-#         #print(table_html)
-#         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+@app.post("/predict")
+async def predict_route(request: Request,file: UploadFile = File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        #print(df)
+        preprocesor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
         
-#     except Exception as e:
-#             raise NetworkSecurityException(e,sys)
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
 
     
 if __name__=="__main__":
